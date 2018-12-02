227b3c21677f6424a550f467f5aeef5008661e7f
Kenj-I <kenji.iwase.5@gmail.com>
2018-12-03 01:32:18 +0900

    [update] side menu url

--- components/commons/sideMenu.vue
+++ components/commons/sideMenu.vue
@@ -22,11 +22,11 @@ export default {
   },
   data: () => ({
     menuList: [
-      { name: 'About', url: 'about' },
-      { name: 'Skills', url: 'skills' },
-      { name: 'Works', url: 'works' },
-      { name: 'Blog', url: 'blog' },
-      { name: 'Contact', url: 'contact' },
+      { name: 'About', url: '/about' },
+      { name: 'Skills', url: '/skills' },
+      { name: 'Works', url: '/works' },
+      { name: 'Blog', url: '/blog' },
+      { name: 'Contact', url: '/contact' },
     ],
   }),
 };
